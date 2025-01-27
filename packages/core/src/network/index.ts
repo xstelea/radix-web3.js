03983d0bffad0e2a040071244605a99fa92c9f7c
Alex Stelea <alex@stelea.io>
2025-01-27 00:17:05 +0000

    refactor: include resource metadata

--- packages/core/src/network/index.ts
+++ packages/core/src/network/index.ts
@@ -89,20 +89,74 @@ export const createRadixNetworkClient = (input: {
       },
     )
 
-  const convertResourcesToBalances = (
+  const convertResourcesToBalances = async (
     resources:
       | FungibleResourcesCollectionItem[]
       | NonFungibleResourcesCollectionItem[],
-  ) =>
-    resources
+  ) => {
+    const BATCH_SIZE = 50
+    const divisibilityMap = new Map<string, number>()
+
+    // Split resources into batches of up to 50 items
+    const resourceBatches = []
+    for (let i = 0; i < resources.length; i += BATCH_SIZE) {
+      resourceBatches.push(resources.slice(i, i + BATCH_SIZE))
+    }
+
+    for (const batch of resourceBatches) {
+      const response =
+        await gatewayApiClient.state.getEntityDetailsVaultAggregated(
+          batch.map((item) => item.resource_address),
+        )
+
+      response.forEach((item) => {
+        if (item.details?.type === 'FungibleResource') {
+          divisibilityMap.set(item.address, item.details.divisibility)
+        }
+      })
+    }
+
+    return resources
       .map((item) => {
-        if (item.aggregation_level === 'Global')
+        if (item.aggregation_level === 'Global') {
+          const { name, symbol } = (
+            item.explicit_metadata?.items || []
+          ).reduce<{
+            name: string
+            symbol: string
+          }>(
+            (acc, metadata) => {
+              if (metadata.value.typed.type === 'String') {
+                return {
+                  ...acc,
+                  [metadata.key]: metadata.value.typed.value,
+                }
+              }
+              return acc
+            },
+            { name: '', symbol: '' },
+          )
           return {
             resourceAddress: item.resource_address,
             amount: `${item.amount}`,
+            name,
+            symbol,
+            divisibility: divisibilityMap.get(item.resource_address) ?? '0',
           }
+        }
       })
-      .filter((item) => item?.amount !== '0')
+      .filter(
+        (
+          item,
+        ): item is {
+          resourceAddress: string
+          amount: string
+          name: string
+          symbol: string
+          divisibility: string
+        } => item?.amount !== '0',
+      )
+  }
 
   const getFungibleTokens = async (
     address: string,
@@ -118,6 +172,9 @@ export const createRadixNetworkClient = (input: {
         cursor: nextCursor,
         at_ledger_state: {
           state_version: stateVersion,
+        },
+        opt_ins: {
+          explicit_metadata: ['name', 'symbol'],
         },
       }
 
@@ -154,6 +211,9 @@ export const createRadixNetworkClient = (input: {
           cursor: nextCursor,
           at_ledger_state: {
             state_version: stateVersion,
+          },
+          opt_ins: {
+            explicit_metadata: ['name', 'symbol'],
           },
         }
 
